6798b2a444086252aecbf498dfcfd80610137396
EstherOwino <essyachieng005@gmail.com>
2026-01-28 15:41:04 +0300

    New commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 #This is my first Python program
 print('I love chips!')
@@ -664,3 +665,19 @@ for key, value in capitals.items():
   print(f"{key}: {value}")
 
 print(capitals)
+
+#How to generate random numbers in Python
+
+
+#print(dir(random)) #for a comprehensive list
+
+low = 1
+high = 100
+options = ("rock", "paper", "scissors")
+cards = ["2", "3", "4", "5", "6","7","8","9","10","J", "Q", "K", "A"]
+number = random.randint(low, high) #for a random whole integer, within the brackets, give a range
+number = random.random()#for a random floating number,it gives a range between 0 and 1 by default
+option = random.choice(options)#you can pick a random choice from a sequence
+random.shuffle(cards)
+
+print(cards)

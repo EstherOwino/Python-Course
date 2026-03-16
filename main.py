3d8c486b0c4d3e05acc1df437d7eeee3af8609ee
EstherOwino <essyachieng005@gmail.com>
2026-03-16 17:03:55 +0300

    funtions and return statements

--- main.py
+++ main.py
@@ -681,3 +681,51 @@ option = random.choice(options)#you can pick a random choice from a sequence
 random.shuffle(cards)
 
 print(cards)
+
+#function = a block of reusable code
+#           place () after the funtion name to invoke/call it 
+
+#to define a function use keyword def then the function_name
+def happy_birthday(name, age): #the data inside () is called parameter
+  print(f"Happy birthday {name}!")
+  print(f"You are {age} years old!")
+  print("Happy birthday to you!")
+  print()
+happy_birthday("Esther Owino", 20) #the data inside () is called argument
+
+def display_invoice(username, amount, due_date):
+  print(f"Hello {username}")
+  print(f"Your bill of ${amount:.2f} is due: {due_date}")
+display_invoice("Esther Owino", 42.50, "01/01")
+
+#return = statement used to end a function
+#          and send a result back to the caller
+def add(x, y):
+  z=x+y
+  return z
+
+def subtract(x, y):
+  z=x-y
+  return z
+
+def multiply(x, y):
+  z=x*y
+  return z
+
+def divide(x, y):
+  z=x/y
+  return z
+print(add(1,2))
+print(subtract(1,2))
+print(multiply(1,2))
+print(divide(1,2))
+
+def create_name(first, last):
+  first = first.capitalize()
+  last = last.capitalize()
+  return first + " " + last
+full_name = create_name("esther", "owino")
+print(full_name)
+
+full_name = create_name("spongebob", "squarepants")
+print(full_name)

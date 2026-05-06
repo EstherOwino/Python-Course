a4c6e945c88e10c9a06e5fabd6522f3e6a9d4361
EstherOwino <essyachieng005@gmail.com>
2026-05-06 23:28:25 +0300

    Match-case statements(switch)

--- main.py
+++ main.py
@@ -938,3 +938,97 @@ print(odd_nums)
 grades = [85, 42, 79, 90, 56, 61, 30]
 passing_grades = [grade for grade in grades if grade >= 60]
 print(passing_grades)
+
+#Match-case statement (switch): An alternative to using many 'elif' statements
+#                               Execute some code if a value matches a 'case'
+#                               Benefits: cleaner and syntax is more readable
+def day_of_week(day):
+  if day == 1:
+    return "It is Sunday"
+  elif day == 2:
+    return "It is Monday"
+  elif day == 3:
+    return "It is Tuesday"
+  elif day == 4:
+    return "It is Wednesday"
+  elif day == 5:
+    return "It is Thursday"
+  elif day == 6:
+    return "It is Friday"
+  elif day == 7:
+    return "It is Saturday"
+  else:
+    return "Not a valid day"
+  
+print(day_of_week(7))
+
+#case = value we are examining eg.day
+def day_of_week(day):
+  match day:
+    case 1:
+      return "It is Sunday"
+    case 2:
+      return "It is Monday"
+    case 3:
+      return "It is Tuesday"
+    case 4:
+      return "It is Wednesday"
+    case 5:
+      return "It is Thursday"
+    case 6:
+      return "It is Friday"
+    case 7:
+      return "It is Saturday"
+    case _:#for else
+      return "Not a valid day"
+  
+print(day_of_week("pizza"))
+
+def is_weekend(day):
+  match day:
+    case "Sunday":
+      return True
+    case "Monday":
+      return False
+    case "Tuesday":
+      return False
+    case "Wednesday":
+      return False
+    case "Thursday":
+      return False
+    case "Friday":
+      return False
+    case "Saturday":
+      return True
+    case _:#for else
+      return False
+print(is_weekend("chips"))
+
+def is_weekend(day):
+  match day:
+    case "Sunday" | "Saturday":
+      return True
+    case "Monday" | "Tuesday" | "Wenesday" | "Thursday" | "Friday":
+      return False
+    case _:#for else
+      return False
+  
+print(is_weekend("Tuesday"))
+
+def grading():
+  marks = int(input("Enter your marks: "))
+  marks = int(marks/10)
+  match marks:
+    case 10 | 9 | 8 | 7:
+      return "You have an A!"
+    case 6:
+      return "You have a B!"
+    case 5:
+      return "You have a C!"
+    case 4:
+      return "You have a D!"
+    case 3 | 2 | 1 | 0:
+      return "You have failed!"
+    case _:
+      return "Invalid marks!"
+print(grading())

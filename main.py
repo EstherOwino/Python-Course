9bf6473ef89250dc2443a85bb5de0b3ff71a7b3b
EstherOwino <essyachieng005@gmail.com>
2026-06-03 10:52:55 +0300

    Static methods

--- main.py
+++ main.py
@@ -1456,3 +1456,35 @@ animals = [Dog(), Cat(), Car()]
 for animal in animals:
   animal.speak()
   print(animal.alive)
+
+
+# Static methods = A method that belong to a class rather than any object from that class(instance)
+#                  Usually used for general utility functions
+# Instance methods = Best for operations on instances of the class (objects)
+# Static methods = Best for utility functions that do not need access to class data
+
+class Employee:
+  def __init__(self, name, position):
+    self.name = name
+    self.position = position
+
+  #INSTANCE METHOD
+  def get_info(self):
+    return f"{self.name} = {self.position}"
+  
+  #STATIC METHOD, best for general utility functions within a class
+  #static methods don't have self, we are not working with any objects created from this class
+  # static methods don't need an object(but can use an object) to be called from a class
+  @staticmethod
+  def is_valid_position(position):
+    valid_positions = ["Manager", "Cashier", "Cook", "Janitor"]
+    return position in valid_positions
+
+print(Employee.is_valid_position("Manager")) 
+employee1 = Employee("Mr.Krabs", "Manager")
+employee2 = Employee("Squidward", "Cashier")
+employee3 = Employee("Spongebob", "Cook")
+
+print(employee1.get_info())
+print(employee2.get_info())
+print(employee3.get_info())

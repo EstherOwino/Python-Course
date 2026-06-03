8ac0cbb300b1575d5c13acfb3e279b5ef14baf2f
EstherOwino <essyachieng005@gmail.com>
2026-06-03 10:18:59 +0300

    Duck typing in Python

--- main.py
+++ main.py
@@ -1428,3 +1428,31 @@ shapes = [Circle(4), Square(5), Triangle(6, 7), Pizza("pepperoni", 15)]
 
 for shape in shapes:
   print(f"{shape.area()}cm^2")
+
+
+#Duck typing" = Another way to achieve polymorphism besides Inheritance
+#               Object must have the minimum necessary attributes/methods
+#               "If it looks like a duck and quacks like a duck, it must be a duck"
+
+class Animal:
+  alive = True
+
+class Dog(Animal):
+  def speak(self):
+    print("WOOF!")
+
+class Cat(Animal):
+  def speak(self):
+    print("MEOW!")
+
+class Car:
+  alive = False
+
+  def speak(self):
+    print("HONK!")
+
+#If our car object doesn't have the method speak or attribute alive, then it doen't have the minimum #necessary attributes/methods to make it an animal hence generates an error  
+animals = [Dog(), Cat(), Car()]
+for animal in animals:
+  animal.speak()
+  print(animal.alive)

f15d7c84489f5379ab905b904a24377b78899062
EstherOwino <essyachieng005@gmail.com>
2026-07-01 13:11:14 +0300

    alarm clock practise

---
+++ alarm_clock_practise.py
@@ -0,0 +1,15 @@
+#Python Alarm Clock
+import time
+import datetime
+
+target_time = datetime.datetime(2026, 7, 1, 13, 0,50)
+ttf = target_time.strftime('%H:%M:%S')
+
+for x in range(0,20):
+  current_time = datetime.datetime.now()
+  ctf = current_time.strftime('%H:%M:%S')
+  time.sleep(1)
+  print(ctf)
+  if ttf == ctf:
+    print("Wake Up!")
+    break
